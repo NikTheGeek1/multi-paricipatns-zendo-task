8f32eaf6cc197b97c7f816eab7c5c41946244623
xcovers <ntheodoropoulos@outlook.com>
2020-04-28 16:58:01 +0300

    'updates'

--- public/js/group/groupchat.js
+++ public/js/group/groupchat.js
@@ -55,7 +55,7 @@ $(document).ready(function(){
 
   // setting up a timer for the participant who joined in first
   // Set the time we're counting down to
-    var countDownSec = 0;
+    var countDownSec = 600;
 
     // Update the count down every 1 second
     waiting_lobby = setInterval(function() {
